2b796c2e600dadbcbe2a36695c046e92cdc12d34
joaofam <joaopereira2213@gmail.com>
2023-07-14 14:36:24 +0100

    fix: removed incorrect meta attributes

--- joaopereira.ie/src/components/layouts/main.js
+++ joaopereira.ie/src/components/layouts/main.js
@@ -2,19 +2,12 @@ import Head from 'next/head'
 import dynamic from 'next/dynamic'
 import NavBar from '../Navbar'
 import { Box, Container } from '@chakra-ui/react'
-// import Footer from '../footer'
-// import VoxelDogLoader from '../voxel-dog-loader'
-
-// const LazyVoxelDog = dynamic(() => import('../voxel-dog'), {
-//   ssr: false,
-//   loading: () => <VoxelDogLoader />
-// })
 
 const Main = ({ children, router }) => {
   return (
     <Box as="main" pb={8}>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Takuya's homepage" />
         <meta name="author" content="Takuya Matsuyama" />
         <meta name="author" content="craftzdog" />
@@ -27,9 +20,9 @@ const Main = ({ children, router }) => {
         <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
         <meta property="og:site_name" content="Takuya Matsuyama" />
         <meta name="og:title" content="Takuya Matsuyama" />
-        <meta property="og:type" content="website" />
+        <meta property="og:type" content="website" /> */}
         {/* <meta property="og:image" content="https://www.craftz.dog/card.png" /> */}
-        <title>Joao Pereira - Homepage</title>
+        <title>Joao Pereira</title>
       </Head>
 
       <NavBar path={router.asPath} />
