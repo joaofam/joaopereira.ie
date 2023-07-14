ab20a1eea0db7e44f500688be7ad353785c05c69
joaofam <joaopereira2213@gmail.com>
2023-07-14 14:41:12 +0100

    feat: relevant information added

--- joaopereira.ie/src/components/layouts/main.js
+++ joaopereira.ie/src/components/layouts/main.js
@@ -7,21 +7,16 @@ const Main = ({ children, router }) => {
   return (
     <Box as="main" pb={8}>
       <Head>
-        {/* <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
-        <meta name="author" content="Takuya Matsuyama" />
-        <meta name="author" content="craftzdog" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Joao's portfolio" />
+        <meta name="author" content="Joao Pereira" />
+        <meta name="author" content="joaoseph" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="Takuya Matsuyama" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama" />
-        <meta name="og:title" content="Takuya Matsuyama" />
-        <meta property="og:type" content="website" /> */}
-        {/* <meta property="og:image" content="https://www.craftz.dog/card.png" /> */}
+        <meta property="og:site_name" content="Joao Pereira" />
+        <meta name="og:title" content="Joao Pereira" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://static.vecteezy.com/system/resources/previews/008/152/576/original/pixel-art-laptop-computer-icon-illustration-free-vector.jpg" />
         <title>Joao Pereira</title>
       </Head>
 
